8b906a09d854477d01ba2622aedf3dfb1b1e29ba
aarkilanian <a.arkilanian@gmail.com>
2019-01-24 11:33:26 -0500

    Initial commit

---
+++ 1.PopSimFinal.R
@@ -0,0 +1,214 @@
+# This script runs a population simulation and generates abundance data
+# on which EWS analyses are performed. This script outputs a RData file 
+# containing the simulated population abundances.
+#
+# Author:  Alex Arkilanian & Gaurav Baruah
+# Date:    January 23, 2019
+#----------------------------------------------------------------------------
+
+# Clean environment
+rm(list = ls())
+
+## Inital parameters of model #####
+
+N.init <- 100 # Initial population size
+r <- 2.35     # Population growth rate
+K <- 100      # Carrying capacity
+h <- 1        # Half-saturation constant
+
+c.list.fold <- c(0, 0.70, 1.20, 1.70)  # Forcing parameters for fold model
+c.list.trans <- c(0, 0.03, 0.05, 0.07) # Forcing parameters for transcritical model
+
+dt.list <- c(0.025, (2 * 0.25), (2 * 0.5), (2 * 1), (2 * 2)) # List of resolutions as fractions of generation time of 2
+
+tmax <- 100 # Max length of time series
+tmin <- 0   # Min length of time series
+
+reps <- 100 # Number of replicates to run
+
+## Setup simulation #####
+
+# Number of data points within each time step for each forcing level
+p.list <- c((-tmin + tmax)/dt.list[1] + 1,        # real-time
+            floor((-tmin + tmax)/dt.list[2] + 1), # 1/4 generation time
+            floor((-tmin + tmax)/dt.list[3] + 1), # 1/2 generation time
+            floor((-tmin + tmax)/dt.list[4] + 1), # 1 generation time
+            floor((-tmin + tmax)/dt.list[5] + 1)) # 2 generaton times
+
+# Forcing in order for fold model
+c.fold <- c(rep(c.list.fold, each = p.list[1]))
+
+# Forcing in order for transcritical model
+c.trans <- c(rep(c.list.trans, each = p.list[1]))
+
+# Time values (in number of timesteps)
+time <- c(rep(seq(tmin, tmax, dt.list[1]), length(c.list.fold)))
+
+# Logical vectors marking when to reinitialize population size to N.init
+reInit <- time == tmin
+reInit.2 <- rep(c(TRUE, rep(FALSE, (p.list[5] - 1))), 9)
+reInit.1 <- rep(c(TRUE, rep(FALSE, (p.list[4] - 1))), 9)
+reInit.50 <- rep(c(TRUE, rep(FALSE, (p.list[3] - 1))), 9)
+reInit.25 <- rep(c(TRUE, rep(FALSE, (p.list[2] - 1))), 9)
+
+# When to sample from high-res to low-res populations
+# Determined as when time point is multiple of resolution step
+TF.2 <- (time %% dt.list[5]) == 0
+TF.1 <- (time %% dt.list[4]) == 0
+TF.50 <- (time %% dt.list[3]) == 0
+TF.25 <- (time %% dt.list[2]) == 0
+
+## Running model at dt = dt.list[1] and subsetting to low-res #####
+
+# Loop initializations
+dt <- dt.list[1]
+
+Ns.fold.2 <- vector("list", reps)
+Ns.trans.2 <- vector("list", reps)
+
+Ns.fold.1 <- vector("list", reps)
+Ns.trans.1 <- vector("list", reps)
+
+Ns.fold.50 <- vector("list", reps)
+Ns.trans.50 <- vector("list", reps)
+
+Ns.fold.25 <- vector("list", reps)
+Ns.trans.25 <- vector("list", reps)
+
+Ns.fold <- vector("list", reps)
+Ns.trans <- vector("list", reps)
+
+# Main loop
+
+for (j in 1:(reps)) {
+  
+  a <- b <- c <- d <- 1
+  
+  fold.dead <- FALSE   
+  trans.dead <- FALSE
+  
+  ### Initialize containers
+  # Real-time containers
+  N.fold <- numeric(p.list[1]*4)
+  N.trans <- numeric(p.list[1]*4)
+  
+  # Quarter generation time containers
+  N.fold.25 <- numeric(p.list[2]*4)
+  N.trans.25 <- numeric(p.list[2]*4)
+  
+  # Half generation time containers
+  N.fold.50 <- numeric(p.list[3]*4)
+  N.trans.50 <- numeric(p.list[3]*4)
+  
+  # Full generation time containers
+  N.fold.1 <- numeric(p.list[4]*4)
+  N.trans.1 <- numeric(p.list[4]*4)
+  
+  # Double generation time containers
+  N.fold.2 <- numeric(p.list[5]*4)
+  N.trans.2 <- numeric(p.list[5]*4)
+  
+  for (i in 1:length(time)) {
+    
+    # Populations initilization and reinitialization
+    if (reInit[i]) {
+      N.fold[i] <- N.init
+      N.trans[i] <- N.init
+      fold.dead <- FALSE
+      trans.dead <- FALSE
+    } else {
+      # Retrieve values here rather than retrieving multiple times in loop
+      curTime <- time[i]        
+      curHarv.fold <- c.fold[i]
+      curHarv.trans <- c.trans[i]
+      
+      ## Fold bifurcation
+      if(fold.dead){} # If pop extinct don't do anything
+      else {
+        N.past <- N.fold[i-1] 
+        growth <- N.past * r * (1 - (N.past / K)) * dt 
+        noise  <- rnorm(1,0,3) * dt
+        harvest <- (curHarv.fold * curTime * N.past^2 / (N.past^2 + h^2)) * dt
+        
+        N.cur <- N.past + growth + noise - harvest
+        
+        # If dead, keep it dead and prevent going into negative pop size
+        if (N.cur < 0) { 
+          fold.dead <- TRUE
+          N.cur <- 0
+        }
+        
+        # Store N
+        N.fold[i]      <- N.cur
+      }
+      
+      ## Transcritical bifurcation
+      if(trans.dead){} # If pop extinct don't do anything
+      else {
+        N.past <- N.trans[i-1]
+        growth <- N.past * r * (1 - (N.past / K)) * dt
+        noise <- rnorm(1,0,3) * dt
+        harvest <- curHarv.trans * curTime * N.past * dt
+        
+        N.cur  <- N.past + growth + noise - harvest
+        
+        # If dead, keep it dead and prevent going into negative pop size 
+        if (N.cur < 0) {
+          trans.dead <- TRUE
+          N.cur <- 0
+        }
+        
+        # Store N
+        N.trans[i]      <- N.cur
+      }
+    }
+    
+    # Population subsetting
+    if(TF.2[i]) {
+      # Store N
+      N.fold.2[a]  <- N.fold[i]
+      N.trans.2[a] <- N.trans[i]
+      a <- a + 1
+    }
+    if(TF.1[i])    {
+      # Store N
+      N.fold.1[b]  <- N.fold[i]
+      N.trans.1[b] <- N.trans[i]
+      b <- b + 1
+    }
+    if(TF.50[i]) {
+      # Store N
+      N.fold.50[c]  <- N.fold[i]
+      N.trans.50[c] <- N.trans[i]
+      c <- c + 1
+    }
+    if(TF.25[i])    {
+      # Store N
+      N.fold.25[d]  <- N.fold[i]
+      N.trans.25[d] <- N.trans[i]
+      d <- d + 1
+    }
+    
+  } 
+  
+  # N results
+  Ns.fold.2[[j]]    <- N.fold.2
+  Ns.fold.1[[j]]    <- N.fold.1
+  Ns.fold.50[[j]]   <- N.fold.50
+  Ns.fold.25[[j]]   <- N.fold.25
+  
+  Ns.trans.2[[j]]   <- N.trans.2
+  Ns.trans.1[[j]]   <- N.trans.1
+  Ns.trans.50[[j]]  <- N.trans.50
+  Ns.trans.25[[j]]  <- N.trans.25
+  
+  Ns.fold[[j]]      <- N.fold
+  Ns.trans[[j]]     <- N.trans
+  
+}
+
+# Clean up and save
+save(  Ns.fold.2, Ns.fold.1, Ns.fold.50, Ns.fold.25,
+       Ns.trans.2, Ns.trans.1, Ns.trans.50, Ns.trans.25,
+       Ns.fold, Ns.trans,
+       file = "AllPop.RData")
